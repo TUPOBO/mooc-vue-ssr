42a3c3a671f836526e7b90cda31a09ab0dc8dd22
TUPOBO <1028461931@qq.com>
2018-08-25 16:25:52 +0800

    实现todo应用界面

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,45 @@
 <template>
-  <div id="text">
-    {{text}}
+  <div id="app">
+    <div id="cover">
+      <Header></Header>
+      <Todo></Todo>
+      <Footer></Footer>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './todo/header.vue'
+import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
   export default {
-    data () {
-      return {
-        text: 'acdf'
-      }
+    components: {
+      Header,
+      Footer,
+      Todo
     }
   }
 </script>
 
-<style scoped>
-  #text {
-    color: red;
+<style lang="stylus" scoped>
+  #app {
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    color #000
   }
+
+  #cover {
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+  background-color #555
+  opacity 0.5
+  z-index -1
+}
+
 </style>
---
+++ src/todo/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="main-header">
+    <h1>JTodo</h1>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="stylus">
+.main-header {
+  text-align center
+  color red
+  font-size 30px
+  }
+</style>
