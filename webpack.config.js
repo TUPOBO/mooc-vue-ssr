acec4df14f8cc91d6e8ab816265227414c4bd49f
TUPOBO <1028461931@qq.com>
2018-08-26 12:20:01 +0800

    webpack配置单独打包css、js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
@@ -9,7 +11,7 @@ const config = {
   mode: process.env.NODE_ENV,
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8]js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -21,27 +23,6 @@ const config = {
       {
         test: /\.jsx$/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true // 直接利用stylus-loader生成的sourceMap，提高编译效率
-            }
-          },
-          'stylus-loader'
-        ]
       },
       {
         test: /\.(gif|jpg|png|svg)$/,
@@ -72,6 +53,20 @@ const config = {
 
 // 开发环境配置
 if (isDev) {
+  config.moudle.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true // 直接利用stylus-loader生成的sourceMap，提高编译效率
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   // source-map 代码映射 便于调试 但是代码量大 运行缓慢
   // eval 代码格式不美观
   config.devtool = '#cheap-module-eval-source-map' // 官方推荐配置
@@ -90,6 +85,27 @@ if (isDev) {
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(), // 热更新插件
     new webpack.NoEmitOnErrorsPlugin() // 减少不需要的信息展示
+  )
+} else {
+  config.output.filename = '[name].[contentHash:8].js'
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true // 直接利用stylus-loader生成的sourceMap，提高编译效率
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    new ExtractPlugin('styles.[chunkhash:8].css')
   )
 }
 
