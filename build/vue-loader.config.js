dd47f42e886d2e2f3bdd21eec9a9610aa0f3c723
TUPOBO <1028461931@qq.com>
2018-08-31 08:36:50 +0800

    单独打包vue文件中的css

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,6 @@
 module.exports = (isDev) => {
   return {
     preserveWhitespace: true, // 删除HTML中多余空格
-    extractCSS: true // 将vue文件中的css样式单独打包出来
+    extractCSS: !isDev // 将vue文件中的css样式单独打包出来
   }
 }
